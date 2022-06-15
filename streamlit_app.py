c3e69adb7d08a18ed346394da9ea7e74c7c87fc5
clayton500k <clayton@the500k.com>
2022-06-15 18:26:15 +0100

    Range import and type handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,90 @@
+from ast import Str
+import google_auth_httplib2
+import httplib2
+import pandas as pd
 import streamlit as st
 from google.oauth2 import service_account
-from gsheetsdb import connect
+from googleapiclient.discovery import build
+from googleapiclient.http import HttpRequest
+from re import sub
+from decimal import Decimal
 
-# Create a connection object.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=[
-        "https://www.googleapis.com/auth/spreadsheets",
-    ],
-)
-conn = connect(credentials=credentials)
+SCOPE = "https://www.googleapis.com/auth/spreadsheets"
+SPREADSHEET_ID = "1uAa3CbD5uYpdEQs3RXenCb5trLqnZGbOtqfaxUhcp0E"
+SHEET_NAME = "Bank"
+GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}"
 
-# Perform SQL query on the Google Sheet.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchmany(10)
-    return rows
+@st.experimental_singleton()
 
-sheet_url = st.secrets["private_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
+def StringToDec(x):
+    if len(x) > 0:
+        return float(x) #Decimal(x)
+    
+    return np.nan
 
-# body
+def connect_to_gsheet():
+    # Create a connection object.
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=[SCOPE],
+    )
+
+    # Create a new Http() object for every request
+    def build_request(http, *args, **kwargs):
+        new_http = google_auth_httplib2.AuthorizedHttp(
+            credentials, http=httplib2.Http()
+        )
+        return HttpRequest(new_http, *args, **kwargs)
+
+    authorized_http = google_auth_httplib2.AuthorizedHttp(
+        credentials, http=httplib2.Http()
+    )
+    service = build(
+        "sheets",
+        "v4",
+        requestBuilder=build_request,
+        http=authorized_http,
+    )
+    gsheet_connector = service.spreadsheets()
+    return gsheet_connector
+
+def get_data(gsheet_connector) -> pd.DataFrame:
+    values = (
+        gsheet_connector.values()
+        .get(
+            spreadsheetId=SPREADSHEET_ID,
+            range=f"{SHEET_NAME}!A1:H6",
+        )
+        .execute()
+    )
+
+    df = pd.DataFrame(values["values"])
+    df.columns = df.iloc[0]
+    df = df[1:]
+    
+    #error handle formats (currency -> float)
+    df = df.fillna(value=np.nan)
+    dfDA = df['Debit Amount'].apply(lambda x: StringToDec(sub(r'[^\d.]', '', x)))
+    df['Debit Amount'] = dfDA
+    dfCA = df['Credit Amount'].apply(lambda x: StringToDec(sub(r'[^\d.]', '', x)))
+    df['Credit Amount'] = dfCA
+
+    return df
+
+gsheet_connector = connect_to_gsheet()
+
+# Body
 st.title('500k Analytics')
 
-# Print results.
-for row in rows:
-    st.write(f"{row.Renamer} gave {row.Debit_Amount}:")
+st.write(f"This app shows how a Streamlit app can interact easily with a [Google Sheet]({GSHEET_URL}) to read or store data.")
 
-#Table
-st.dataframe(rows)
+#expander = st.expander("See all records")
+#with expander:
 
+data = get_data(gsheet_connector)
+
+st.dataframe(data)
+
+print(data)
+
+#st.title(data[''])
