7196cfbc9d52366a20f0a77d8a859b9d86650b29
clayton500k <clayton@the500k.com>
2022-06-21 17:07:35 +0100

    Donor Comparison Restructure

--- pages/4_📊_Donor_Comparison.py
+++ pages/4_📊_Donor_Comparison.py
@@ -6,18 +6,15 @@ import plotly.graph_objects as go
 from st_aggrid import AgGrid
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 
-# Select Year
-
-# New Donors
-
-# Increased 
-
-# Decreased
-
 def dc_page():
-   
+
+    #Horizontal Radio
+    st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+
+    view = st.radio('Choose View:',['Lollipop','Tables'])
+
     # User Input Year
-    year_list = [2018,2019,2020,2021,2022]
+    year_list = [2017,2018,2019,2020,2021,2022]
     select_year = st.selectbox('Select Year',year_list,year_list.index(2022))
 
     DM = st.session_state.DM
@@ -36,66 +33,64 @@ def dc_page():
     #Sort by absolute values
     output = output.reindex(output['Delta'].abs().sort_values(ascending=False).index)
 
-    #output.columns = output.columns.astype(str)
+    if view=='Lollipop':
 
-    # Plot Lollipop Chart
-    #https://towardsdatascience.com/lollipop-dumbbell-charts-with-plotly-696039d5f85
+        #https://towardsdatascience.com/lollipop-dumbbell-charts-with-plotly-696039d5f85
 
-    st.subheader("Change from previous year's giving:")
-    
-    x = st.slider('', min_value=-60000, max_value=60000,value=[1000,60000])
-
-    filtered_output = output[(output['Delta']>=x[0]) & (output['Delta']<=x[1])]
-
-    filtered_output = filtered_output.reindex(filtered_output['Delta'].abs().sort_values().index)
-
-    syd = filtered_output[select_year].tolist() #Select year data
-    sydm1 = filtered_output[(select_year-1)].tolist() #Select year data minus 1 
-
-    fig3= go.Figure()
-    fig3.add_trace(go.Scatter(x = syd, 
-                            y = filtered_output['Renamer'],
-                            mode = 'markers',
-                            marker_color = 'darkblue',
-                            marker_size = 10,
-                            name = select_year))
-    fig3.add_trace(go.Scatter(x = sydm1, 
-                            y = filtered_output['Renamer'],
-                            mode = 'markers',
-                            marker_color = 'darkorange', 
-                            marker_size = 10,
-                            name = (select_year-1)))
-    for i in range(0, len(syd)):
-                fig3.add_shape(type='line',
-                                x0 = syd[i],
-                                y0 = i,
-                                x1 = sydm1[i],
-                                y1 = i,
-                                line=dict(color='crimson', width = 3))
-
-    st.plotly_chart(fig3,use_containter_width=True)
-    
-    st.table(filtered_output.sort_values(by=['Delta'],ascending=False).style.set_precision(2))
-
-    st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-
-    type = st.radio("View List of",['New','Increased','Lost','Decreased'])
-
-    if type=='New':
-        st.subheader(f'New donors in {select_year}')
-        st.table(output[(output[select_year]>0) & (output[(select_year-1)]==0)].style.set_precision(2))
-    elif type=='Increased':
-        st.subheader(f'Increased donors in {select_year}')
-        st.table(output[output['Delta']>0].style.set_precision(2))
-    elif type=='Lost':
-        st.subheader(f'Lost donors in {select_year}')
-        st.table(output[(output[select_year]==0) & (output[(select_year-1)]>0)].style.set_precision(2))
-    else:
-        st.subheader(f'Decreased donors in {select_year}')
-        st.table(output[output['Delta']<0].style.set_precision(2))
+        st.subheader("Change from previous year's giving:")
         
-st.set_page_config(page_title="Donor Comparison", page_icon="📊")
+        x = st.slider('', min_value=-60000, max_value=60000,value=[1000,60000])
 
+        filtered_output = output[(output['Delta']>=x[0]) & (output['Delta']<=x[1])]
+
+        filtered_output = filtered_output.reindex(filtered_output['Delta'].abs().sort_values().index)
+
+        syd = filtered_output[select_year].tolist() #Select year data
+        sydm1 = filtered_output[(select_year-1)].tolist() #Select year data minus 1 
+
+        fig3= go.Figure()
+        fig3.add_trace(go.Scatter(x = syd, 
+                                y = filtered_output['Renamer'],
+                                mode = 'markers',
+                                marker_color = 'darkblue',
+                                marker_size = 10,
+                                name = select_year))
+        fig3.add_trace(go.Scatter(x = sydm1, 
+                                y = filtered_output['Renamer'],
+                                mode = 'markers',
+                                marker_color = 'darkorange', 
+                                marker_size = 10,
+                                name = (select_year-1)))
+        for i in range(0, len(syd)):
+                    fig3.add_shape(type='line',
+                                    x0 = syd[i],
+                                    y0 = i,
+                                    x1 = sydm1[i],
+                                    y1 = i,
+                                    line=dict(color='crimson', width = 3))
+        fig3 = fig3.update_layout(height=400,legend=dict(orientation="h",y=-0.15,x=0.15),margin=dict(t=10,b=10))
+        
+        st.plotly_chart(fig3,use_containter_width=True)
+        
+        st.table(filtered_output.sort_values(by=['Delta'],ascending=False).style.set_precision(2))
+
+    else:
+
+        type = st.radio("View List of",['New','Increased','Lost','Decreased'])
+
+        if type=='New':
+            st.subheader(f'New donors in {select_year}')
+            st.table(output[(output[select_year]>0) & (output[(select_year-1)]==0)].style.set_precision(2))
+        elif type=='Increased':
+            st.subheader(f'Increased donors in {select_year}')
+            st.table(output[output['Delta']>0].style.set_precision(2))
+        elif type=='Lost':
+            st.subheader(f'Lost donors in {select_year}')
+            st.table(output[(output[select_year]==0) & (output[(select_year-1)]>0)].style.set_precision(2))
+        else:
+            st.subheader(f'Decreased donors in {select_year}')
+            st.table(output[output['Delta']<0].style.set_precision(2))
+        
 st.title('Donor Comparison')
 
 if 'password_check' in st.session_state:
